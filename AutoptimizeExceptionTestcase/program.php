e826b55753561a4423b42a1f4f7a2bcd97b13292
Daniel Llewellyn <daniel@bowlhat.net>
2019-03-28 16:36:51 +0000

    Add project files.

---
+++ AutoptimizeExceptionTestcase/program.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+function str_is_valid_regex( $string )
+{
+    set_error_handler( function() {}, E_WARNING );
+    $is_regex = ( false !== preg_match( $string, '' ) );
+    restore_error_handler();
+
+    return $is_regex;
+}
+
+print_r(str_is_valid_regex('<!--[if'));
